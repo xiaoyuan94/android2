fc9b16a6bac6183ff6a0dca63a7f6a09e586ce14
xiaoyuan94 <402973804@qq.com>
2020-03-10 17:25:36 +0800

    修改数据库manger升级设置

--- project/app/src/main/java/com/xxyuan/project/db/MyOpenHelper.java
+++ project/app/src/main/java/com/xxyuan/project/db/MyOpenHelper.java
@@ -30,7 +30,6 @@ public class MyOpenHelper extends DaoMaster.OpenHelper {
 
         Log.i("version", oldVersion + "---先前和更新之后的版本---" + newVersion);
         if (oldVersion < newVersion) {
-            Log.i("version", oldVersion + "---先前和更新之后的版本---" + newVersion);
 //            MigrationHelper.getInstance().migrate(db, StudentDao.class);
             //更改过的实体类(新增的不用加)   更新UserDao文件 可以添加多个  XXDao.class 文件
             MigrationHelper.migrate(db, new MigrationHelper.ReCreateAllTableListener() {

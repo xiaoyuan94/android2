4c35abc0cde72eabc83e527e7753241e95df0b43
xiaoyuan94 <402973804@qq.com>
2020-09-14 14:09:37 +0800

    新增横向刷新

--- project/app/src/main/java/com/xxyuan/project/ui/smartrefresh/SmartRefreshActivity.java
+++ project/app/src/main/java/com/xxyuan/project/ui/smartrefresh/SmartRefreshActivity.java
@@ -5,16 +5,21 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.scwang.smart.refresh.footer.ClassicsFooter;
-import com.scwang.smart.refresh.header.ClassicsHeader;
+import com.scwang.smart.refresh.header.MaterialHeader;
+import com.scwang.smart.refresh.horizontal.HorizontalFooter;
+import com.scwang.smart.refresh.horizontal.HorizontalHeader;
+import com.scwang.smart.refresh.horizontal.SmartRefreshHorizontal;
 import com.scwang.smart.refresh.layout.SmartRefreshLayout;
 import com.scwang.smart.refresh.layout.api.RefreshLayout;
 import com.scwang.smart.refresh.layout.listener.OnLoadMoreListener;
 import com.scwang.smart.refresh.layout.listener.OnRefreshListener;
+import com.scwang.smart.refresh.layout.wrapper.RefreshFooterWrapper;
 import com.xxyuan.project.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static com.xxyuan.project.app.XxyuanApplication.getContext;
 
 public class SmartRefreshActivity extends AppCompatActivity {
 
@@ -22,6 +27,10 @@ public class SmartRefreshActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     @BindView(R.id.refreshLayout)
     SmartRefreshLayout refreshLayout;
+    @BindView(R.id.recyclerView2)
+    RecyclerView recyclerView2;
+    @BindView(R.id.refreshLayout2)
+    SmartRefreshHorizontal refreshLayout2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +51,25 @@ public class SmartRefreshActivity extends AppCompatActivity {
             @Override
             public void onLoadMore(RefreshLayout refreshlayout) {
                 refreshlayout.finishLoadMore(2000/*,false*/);//传入false表示加载失败
+            }
+        });
+
+//        refreshLayout2.setRefreshHeader(new MaterialHeader(getContext()));
+//        refreshLayout2.setRefreshFooter(new RefreshFooterWrapper(new MaterialHeader(getContext())), -1, -2);
+
+        refreshLayout2.setRefreshHeader(new ClassicsHeader(getContext()));
+        refreshLayout2.setRefreshFooter(new RefreshFooterWrapper(new ClassicsHeader(getContext())), -1, -2);
+
+        refreshLayout2.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                refreshLayout2.finishRefresh(2000/*,false*/);//传入false表示刷新失败
+            }
+        });
+        refreshLayout2.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(RefreshLayout refreshlayout) {
+                refreshLayout2.finishLoadMore(2000/*,false*/);//传入false表示加载失败
             }
         });
     }
